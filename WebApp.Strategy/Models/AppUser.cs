f8e8882d37eb1d1d678108d256d2ad5717fe363a
Murat Bolulu <murat.bolulu@digilera.com>
2024-09-09 02:23:28 +0300

    strategy pattern initial

---
+++ WebApp.Strategy/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BaseProject.Models
+{
+    public class AppUser: IdentityUser
+    {
+        public int Age { get; set; }
+        //ihtiyaca göre içerisini ekleyeceğim.
+    }
+}
