b0e7a876cfb926bd2903c2ca7c221b8b55d84476
Murat Bolulu <murat.bolulu@digilera.com>
2024-09-14 16:02:48 +0300

    template design patterns is build.

---
+++ WebApp.Template/Models/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProject.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<AppUser> AppUsers1 { get; set; }
+    }
+}
---
+++ WebApp.Template/UserCards/UserCardTemplate.cs
@@ -0,0 +1,37 @@
+﻿using BaseProject.Models;
+using System.Text;
+
+namespace WebApp.Template.UserCards
+{
+    public abstract class UserCardTemplate
+    {
+        protected AppUser _appUser { get; set; }
+
+        public void SetUser(AppUser appUser)
+        {
+            _appUser = appUser;
+        }
+
+        //işin hangi sırayla çalışacağını belirler, build eder.
+        public string Build()
+        {
+            if (_appUser is null) throw new ArgumentNullException(nameof(_appUser));
+
+            var sb = new StringBuilder();
+            sb.Append("<div class='card'>");
+            sb.Append(SetPicture());
+            sb.Append($@"<div class='card-body'>
+                        <h5> {_appUser.UserName}
+                        <p>{_appUser.Description}</p>");
+            sb.Append(SetFooter());
+            sb.Append("</div>");
+            sb.Append("</div>");
+
+            return sb.ToString();
+
+        }
+
+        protected abstract string SetFooter();
+        protected abstract string SetPicture();
+    }
+}
